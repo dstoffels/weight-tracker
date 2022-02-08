a5ac9c2e9465a1d8bdf98ead0a1214ecf9f1d7f2
dstoffels <dan.stoffels@gmail.com>
2022-02-08 10:47:35 -0600

    Created DisplayEntries.jsx, table displayed

---
+++ src/Components/DisplayEntries/DisplayEntries.jsx
@@ -0,0 +1,28 @@
+const DisplayEntries = props => {
+	const { parentEntries } = props;
+
+	return (
+		<table>
+			<thead>
+				<tr>
+					<th>Entry No.</th>
+					<th>Weight</th>
+					<th>Date</th>
+				</tr>
+			</thead>
+			<tbody>
+				{parentEntries.map(({ weight, date }, i) => {
+					return (
+						<tr key={i}>
+							<td>{i + 1}</td>
+							<td>{weight}</td>
+							<td>{date}</td>
+						</tr>
+					);
+				})}
+			</tbody>
+		</table>
+	);
+};
+
+export default DisplayEntries;
